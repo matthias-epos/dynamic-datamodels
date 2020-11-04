811ba0df1af2786127d9fb767b1a36ab7857b6af
Matthias Strauss <matthias.strauss@epos-cat.de>
2020-11-04 12:48:43 +0100

    Added README and reminders not to upload credentials

--- src/main/java/de/eposcat/master/connection/CustomOracleConnectionManager.java
+++ src/main/java/de/eposcat/master/connection/CustomOracleConnectionManager.java
@@ -9,29 +9,12 @@ import oracle.jdbc.pool.OracleDataSource;
 public class CustomOracleConnectionManager extends AbstractConnectionManager {
 
     public CustomOracleConnectionManager(RelationalApproach approach) {
+        // DON'T SHARE NON LOCALHOST SERVERS AND CREDENTAILS ON GIT!
         connectionString = "jdbc:oracle:thin:@//localhost:33333/xepdb1";
-        
-        
-        
-//        switch(approach) {
-//            case EAV:
-//                connectionString += "test_eav";
-//                break;
-//            //Wird gestrichen?
-//            case TABLE_PER_TYPE:
-//                throw new RuntimeException("Not supported type");
-//            case JSON:
-//                connectionString += "test_json";
-//                break;
-//            case KEY_VALUE_STORE:
-//                throw new RuntimeException("Not supported type");
-//               
-//        }
         
         databaseProperties = new Properties();
         databaseProperties.put("user", "test");
         databaseProperties.put("password", "test");
-//        databaseProperties.put("url", connectionString);
     }
     
     public Connection createDatabaseConnection() {
--- src/main/java/de/eposcat/master/connection/PostgresConnectionManager.java
+++ src/main/java/de/eposcat/master/connection/PostgresConnectionManager.java
@@ -2,26 +2,28 @@ package de.eposcat.master.connection;
 
 import java.util.Properties;
 
+
 public class PostgresConnectionManager extends AbstractConnectionManager {
 
     public PostgresConnectionManager(RelationalApproach approach) {
+        // DON'T SHARE NON LOCALHOST SERVERS AND CREDENTAILS ON GIT!!
         connectionString = "jdbc:postgresql://localhost/";
-        
-        switch(approach) {
+
+        switch (approach) {
             case EAV:
                 connectionString += "test_eav";
-                break;
-            //Wird gestrichen?
+            break;
+            // remove this approach?
             case TABLE_PER_TYPE:
                 throw new RuntimeException("Not supported type");
             case JSON:
                 connectionString += "test_json";
-                break;
+            break;
             case KEY_VALUE_STORE:
                 throw new RuntimeException("Not supported type");
-               
+
         }
-        
+
         databaseProperties = new Properties();
         databaseProperties.put("user", "postgres");
         databaseProperties.put("password", "admin");
