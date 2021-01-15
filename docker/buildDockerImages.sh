892ce9de796cb45e6b75de4925d54658ccf5e0da
Matthias Strauss <matthias.strauss@epos-cat.de>
2021-01-15 07:31:18 +0100

    Added script and adapted mvn for building images on unix, changed git attributes to stop changing lineendings in shell scripts

---
+++ docker/buildDockerImages.sh
@@ -0,0 +1,13 @@
+echo OFF
+
+echo If creating/starting the images/containers fails, make sure to use linux-style line endings in the shell scripts!
+
+echo Building postgres image
+docker build -t mstrepos1/dynamic_datamodels:postgres --rm ./postgres-docker/
+
+echo Building vanilla oracle image which ours is based on \(not available in repo\)
+docker build -t oracle/database:18.4.0-xe -f Dockerfile.xe --rm ./vanilla-oracle-docker/18.4.0/
+
+echo Building oracle image
+docker build -t mstrepos1/dynamic_datamodels:oracle --rm ./oracle-docker/
+
